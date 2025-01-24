f492be9ca68e75026a06d6e7ecba0e00cc5fb675
Krzysztof Perwel <kperwel@evolution.com>
2025-01-24 19:39:58 +0100

    Handle mouse camera movement

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,14 +1,41 @@
 extends CharacterBody3D
 
+@export_subgroup("Movement")
+@export var SPEED = 5
+@export var JUMP_VELOCITY = 8
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@onready var camera = $Camera3D;
+
+var mouse_sensitivity = 700
+var gamepad_sensitivity := 0.075
+
+var mouse_captured := true
+
+var movement_velocity: Vector3
+var rotation_target: Vector3
+var input_mouse: Vector2
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event):
+	if event is InputEventMouseMotion and mouse_captured:
+		input_mouse = event.relative / mouse_sensitivity
+		
+		rotation_target.y -= event.relative.x / mouse_sensitivity
+		rotation_target.x -= event.relative.y / mouse_sensitivity
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	# Rotation
+	camera.rotation.z = lerp_angle(camera.rotation.z, -input_mouse.x * 25 * delta, delta * 5)	
+	camera.rotation.x = lerp_angle(camera.rotation.x, rotation_target.x, delta * 25)
+	rotation.y = lerp_angle(rotation.y, rotation_target.y, delta * 25)
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
